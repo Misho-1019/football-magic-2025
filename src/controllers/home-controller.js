2745ab7711461a8d4cddb8655d1ceea00fb1e991
Misho1019 <mikailtorres99@gmail.com>
2025-01-20 03:34:43 +0200

    Add dynamic rendering on home page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import players from "../players.js";
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { players })
 })
 
 router.get('/about', (req, res) => {
