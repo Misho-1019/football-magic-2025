949c985c572ce7d06962ed56e25e05f6f70a66cb
Misho1019 <mikailtorres99@gmail.com>
2025-02-04 04:06:48 +0200

    Add login page

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,23 @@
 import User from "../models/User.js"
+import bcrypt from "bcrypt";
 
 export default {
     register(userData) {
         return User.create(userData)
+    },
+    async login(email, password) {
+        const user = await User.find({ email })
+
+        if (!user) {
+            throw new Error('Invalid email or password')
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        
+        if (!isValid) {
+            throw new Error('Invalid email or password')
+        }
+
+        return;
     }
 }
