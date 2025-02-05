5a1bd4cc5df752d66da4ff578b9bf8aaf9e12deb
Misho1019 <mikailtorres99@gmail.com>
2025-02-05 03:33:02 +0200

    Create dynamic navigation

--- src/controllers/player-controller.js
+++ src/controllers/player-controller.js
@@ -28,7 +28,7 @@ playerController.get('/:playerId/details', async (req, res) => {
     const playerId = req.params.playerId;
 
     const player = await playerService.findPlayerWithCasts(playerId);
-    const isCreator = player.creator && player.creator.toString() === req.user?.id
+    const isCreator = player.creator?.equals(req.user?.id)
 
     res.render('player/details', { player, isCreator })
 })
